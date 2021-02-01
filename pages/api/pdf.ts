1afc6c40cad761ca3d5783d8f54e84ae2969f4d0
Cody George <cody.george@airtasker.com>
2021-02-01 14:54:17 +1100

    no promise

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -5,17 +5,15 @@ import SVGtoPDF from "svg-to-pdfkit";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    return new Promise<void>((resolve) => {
-      const doc = new PDFDocument({ layout: "landscape" });
+    const doc = new PDFDocument({ layout: "landscape" });
 
-      const svg = req.body.svg;
-      doc.text(`Sociogram: ${req.body.title}`, 20, 20);
-      SVGtoPDF(doc, svg, 0, 20);
-      res.setHeader("Content-Disposition", "attachment;filename=file.pdf");
-      res.setHeader("Content-Type", "application/pdf");
-      res.statusCode = 200;
-      doc.pipe(res);
-      doc.end();
-    });
+    const svg = req.body.svg;
+    doc.text(`Sociogram: ${req.body.title}`, 20, 20);
+    SVGtoPDF(doc, svg, 0, 20);
+    res.setHeader("Content-Disposition", "attachment;filename=file.pdf");
+    res.setHeader("Content-Type", "application/pdf");
+    res.statusCode = 200;
+    doc.pipe(res);
+    doc.end();
   }
 };
